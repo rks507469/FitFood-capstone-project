7036273a40e6aca0033f5c53037de76ad1159e5b
Anurag Singh <siinganurag786@gmail.com>
2022-05-25 06:11:13 +0530

    Fav recipe list is stored for particular user in firebase

--- app/src/main/java/ahmux/nutritionpoint/recipeAdapter.java
+++ app/src/main/java/ahmux/nutritionpoint/recipeAdapter.java
@@ -5,10 +5,18 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.like.LikeButton;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -17,11 +25,14 @@ public class recipeAdapter extends RecyclerView.Adapter<recipeAdapter.Viewholder
 
     private Context context;
     private ArrayList<recipe_card_model> cardModelArrayList;
+    private DatabaseReference ref;
+    private int n=1;
 
     // Constructor
     public recipeAdapter(Context context,ArrayList<recipe_card_model> cardModelArrayList) {
         this.context = context;
         this.cardModelArrayList = cardModelArrayList;
+
     }
 
     @NonNull
@@ -33,6 +44,7 @@ public class recipeAdapter extends RecyclerView.Adapter<recipeAdapter.Viewholder
     }
     @Override
     public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+        ref= FirebaseDatabase.getInstance().getReference().child("Fav").child("user");
         recipe_card_model model= cardModelArrayList.get(position);
         holder.recipeName.setText(model.getRecipeName());
         holder.calories.setText(model.getCalories());
@@ -40,6 +52,7 @@ public class recipeAdapter extends RecyclerView.Adapter<recipeAdapter.Viewholder
         holder.servings.setText(model.getServing());
         holder.fats.setText(model.getFat());
         holder.protein.setText(model.getProtein());
+
         // holder.recipeImage.setImageURI((URI)model.getRecipeImg());
         Picasso.get().load((model.getRecipeImg())).into(holder.recipeImage);
         holder.itemView.setOnClickListener(new View.OnClickListener()
@@ -49,6 +62,19 @@ public class recipeAdapter extends RecyclerView.Adapter<recipeAdapter.Viewholder
                 Uri uri= Uri.parse(model.getRecipeurl());
                 Intent intent= new Intent(Intent.ACTION_VIEW,uri);
                 view.getContext().startActivity(intent);
+            }
+        });
+        holder.like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String c="recipe"+String.valueOf(n);
+                ref.child("recipe").push().setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(context.getApplicationContext(), "Liked",Toast.LENGTH_LONG).show();
+                        n++;
+                    }
+                });
             }
         });
     }
@@ -61,16 +87,12 @@ public class recipeAdapter extends RecyclerView.Adapter<recipeAdapter.Viewholder
         // of card items in recycler view.
         return cardModelArrayList.size();
     }
-    public synchronized   void refreshAdapter(ArrayList<recipe_card_model> items) {
-        cardModelArrayList.clear();
-        cardModelArrayList.addAll(items);
-        notifyDataSetChanged();
-    }
     // View holder class for initializing of
     // your views such as TextView and Imageview.
     public class Viewholder extends RecyclerView.ViewHolder {
         private ImageView recipeImage;
         private TextView recipeName, calories,servings,fats,protein,mealtype;
+        private LikeButton like;
 
         public Viewholder(@NonNull View itemView) {
             super(itemView);
@@ -81,6 +103,7 @@ public class recipeAdapter extends RecyclerView.Adapter<recipeAdapter.Viewholder
             fats = itemView.findViewById(R.id.fats);
             protein = itemView.findViewById(R.id.protein);
             mealtype = itemView.findViewById(R.id.mealtype);
+            like=itemView.findViewById(R.id.star_button);
         }
     }
 }
